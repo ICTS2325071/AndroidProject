e79f2d832078b7408fa574e73d33973674595ff8
ICTS2325071 <mattia.cellerino@edu.itspiemonte.it>
2024-10-18 11:26:09 +0200

    update prova gg

--- app/src/main/java/com/example/teamateapplication/MainActivity.kt
+++ app/src/main/java/com/example/teamateapplication/MainActivity.kt
@@ -8,16 +8,10 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        //setContentView(R.layout.activity_main)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        binding.testButton.setOnClickListener{
 
-        }
     }
 }
