a0495a15da88cc460f2a8cc24f15040dac659622
ICTS2325071 <mattia.cellerino@edu.itspiemonte.it>
2024-11-29 12:53:13 +0100

    celle work 29/11

--- app/src/main/java/com/example/teamateapplication/HomeLoginActivity.kt
+++ app/src/main/java/com/example/teamateapplication/HomeLoginActivity.kt
@@ -1,4 +1,12 @@
 package com.example.teamateapplication
 
-class HomeLoginActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeLoginActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_login)
+    }
 }
