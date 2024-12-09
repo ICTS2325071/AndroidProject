a57317f4d7317b308292ae2b73a051fea99a2819
DaniPosse10 <daniel.possemato@gmail.com>
2024-12-09 18:00:56 +0100

    work posse (nulla)

--- app/src/main/java/com/example/teamateapplication/HomePageActivity.kt
+++ app/src/main/java/com/example/teamateapplication/HomePageActivity.kt
@@ -1,12 +1,19 @@
 package com.example.teamateapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class HomePageActivity : AppCompatActivity() {
+
+class  HomePageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+        val  CustomButtonStatistics = findViewById<Button>(R.id.button_statistics)
+        CustomButtonStatistics.setOnClickListener {
+            loadActivity(StatisticsPageActivity())
+        }
     }
 }
