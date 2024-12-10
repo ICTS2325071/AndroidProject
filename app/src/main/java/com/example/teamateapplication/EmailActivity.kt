b8e40abc8fd43c9493fa856952911bb709b667b3
ICTS2325071 <mattia.cellerino@edu.itspiemonte.it>
2024-12-10 17:50:32 +0100

    celle work 10/12 (2)

---
+++ app/src/main/java/com/example/teamateapplication/EmailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.teamateapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EmailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email)
+    }
+}
