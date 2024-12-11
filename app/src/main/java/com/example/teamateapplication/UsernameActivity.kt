2e6fb2c533e29707c6cc02410e158500d102542c
ICTS2325071 <mattia.cellerino@edu.itspiemonte.it>
2024-12-11 11:15:31 +0100

    celle work 11/12

--- app/src/main/java/com/example/teamateapplication/UsernameActivity.kt
+++ app/src/main/java/com/example/teamateapplication/UsernameActivity.kt
@@ -1,6 +1,9 @@
 package com.example.teamateapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +14,11 @@ class UsernameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_username)
+
+        val linearLayoutBack = findViewById<LinearLayout>(R.id.back)
+
+        linearLayoutBack.setOnClickListener {
+            finish()
+        }
     }
 }
