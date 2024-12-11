85e0046ed18a26371d05903cd8b04c36e0e63d43
DaniPosse10 <daniel.possemato@gmail.com>
2024-12-11 11:14:37 +0100

    work posse

--- app/src/main/java/com/example/teamateapplication/StatisticsPageActivity.kt
+++ app/src/main/java/com/example/teamateapplication/StatisticsPageActivity.kt
@@ -13,58 +13,8 @@ class StatisticsPageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statistics_page)
 
-        val buttonSinner: Button = findViewById(R.id.button_sinner)
-        val buttonZverev: Button = findViewById(R.id.button_zverev)
-        val buttonAlcaraz:  Button = findViewById(R.id.button_alcaraz)
-        val buttonFritz: Button = findViewById(R.id.button_fritz)
-        val buttonMedvedev: Button = findViewById(R.id.button_medvedv)
 
 
 
-        val buttonHome = findViewById<ImageButton>(R.id.imageButton_home)
-        val buttonStar = findViewById<ImageButton>(R.id.imageButton_favourites)
-        val buttonSettings = findViewById<ImageButton>(R.id.imageButton_settings)
-
-        buttonHome.setOnClickListener {
-            val intent = Intent(this, HomePageActivity::class.java)
-            startActivity(intent)
-        }
-
-        buttonStar.setOnClickListener {
-            val intent = Intent(this, PreferitiActivity::class.java)
-            startActivity(intent)
-        }
-
-        buttonSettings.setOnClickListener {
-            val intent = Intent(this, ImpostazioniActivity::class.java)
-            startActivity(intent)
-        }
-
-        buttonSinner.setOnClickListener {
-            val intent = Intent(this, PlayerInformationsActivity::class.java)
-            startActivity(intent)
-        }
-
-        buttonZverev.setOnClickListener {
-            val intent = Intent(this, PlayerInformationsActivity::class.java)
-                startActivity(intent)
-            }
-
-        buttonAlcaraz.setOnClickListener {
-            val intent = Intent(this, PlayerInformationsActivity::class.java)
-                    startActivity(intent)
-                }
-
-        buttonFritz.setOnClickListener {
-            val intent = Intent(this, PlayerInformationsActivity::class.java)
-                        startActivity(intent)
-                    }
-
-        buttonMedvedev.setOnClickListener {
-            val intent = Intent(this, PlayerInformationsActivity::class.java)
-                            startActivity(intent)
-                        }
-
-        }
-
-                 }
+    }
+}
